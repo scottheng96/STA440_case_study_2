28d261a98c898f0fb464c7dc81f6ddc771a65398
Ethan Shen <escarmel1@gmail.com>
2020-09-15 21:42:33 -0400

    reads in pickle file and outputs chest sensor data as csv

---
+++ data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+from itertools import chain
+# This function basically un-nests the nested arrays within the pickle format
+def flatten(listOfLists):
+    "Flatten one level of nesting"
+    return chain.from_iterable(listOfLists)
+
+#os.listdir("/Users/ethanshen/Downloads/WESAD")
+#data = pd.read_pickle("/Users/ethanshen/Documents/College/Fa20/STA 440/S2/S2.pkl")
+
+#df = pd.DataFrame()
+
+files = ['S10', 'S11', 'S13', 'S14', 'S15', 'S16', 'S17', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9']
+#path = "/Users/ethanshen/Downloads/WESAD" 
+path = "/hpc/group/sta440-f20/WESAD/WESAD"
+def get_chest_pickle(file_list):
+    chest_data = pd.DataFrame()
+    for file in file_list:
+        file_name = "".join([path, "/", file, "/", file, ".pkl"])
+        data = pd.read_pickle(file_name)
+        
+        df = pd.DataFrame()
+
+        df['ACC_chest_X'] = data['signal']['chest']['ACC'][:,0]
+        df['ACC_chest_Y'] = data['signal']['chest']['ACC'][:,1]
+        df['ACC_chest_Z'] = data['signal']['chest']['ACC'][:,2]
+        df['EMG'] = list(flatten(data['signal']['chest']['EMG']))
+        df['ECG'] = list(flatten(data['signal']['chest']['ECG']))
+        df['EDA'] = list(flatten(data['signal']['chest']['EDA']))
+        df['Temp'] = list(flatten(data['signal']['chest']['Temp']))
+        df['Resp'] = list(flatten(data['signal']['chest']['Resp']))
+        df['Label'] = data['label']
+        df['Participant'] = file
+        chest_data = pd.concat([chest_data, df])
+    return chest_data
+
+
+chest_metrics_df = get_chest_pickle(files)
+
+print(chest_metrics_df)
