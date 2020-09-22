c489184d6827cc157631cf64efc7dea045d2b7c0
mpahren <mep75@duke.edu>
2020-09-22 17:32:39 -0400

    Added in Temp calculations

--- data-feature-engineering.R
+++ data-feature-engineering.R
@@ -6,6 +6,8 @@ chest_wrist <- readRDS("chest_and_wrist.Rds")
 EDA_chest_list = list()
 EDA_wrist_list = list()
 EMG_list = list()
+Temp_chest_list = list()
+Temp_wrist_list = list()
 
 for (i in 1:(chest_wrist$Participant %>% unique() %>% length())) {
   p <- chest_wrist$Participant %>% unique() %>% .[i]
@@ -59,21 +61,61 @@ for (i in 1:(chest_wrist$Participant %>% unique() %>% length())) {
     range_EMG = max_EMG - min_EMG
   )
   
+  # wrist Temp
+  mean_Temp_wrist <- rollapply(subject_p$TEMP_wrist, 240, mean)
+  sd_Temp_wrist <- rollapply(subject_p$TEMP_wrist, 240, sd)
+  max_Temp_wrist <- rollapply(subject_p$TEMP_wrist, 240, max)
+  min_Temp_wrist <- rollapply(subject_p$TEMP_wrist, 240, min)
+  
+  Temp_wrist_df <- tibble(
+    participant = p,
+    label = label,
+    # chest EDA
+    mean_Temp_wrist = mean_Temp_wrist, 
+    sd_Temp_wrist = sd_Temp_wrist,
+    min_Temp_wrist = min_Temp_wrist,
+    max_Temp_wrist = max_Temp_wrist,
+    range_Temp_wrist = max_Temp_wrist - min_Temp_wrist
+  )
+  
+  # chest Temp
+  mean_Temp_chest <- rollapply(subject_p$Temp, 240, mean)
+  sd_Temp_chest <- rollapply(subject_p$Temp, 240, sd)
+  max_Temp_chest <- rollapply(subject_p$Temp, 240, max)
+  min_Temp_chest <- rollapply(subject_p$Temp, 240, min)
+  
+  Temp_chest_df <- tibble(
+    participant = p,
+    label = label,
+    # chest EDA
+    mean_Temp_chest = mean_Temp_chest, 
+    sd_Temp_chest = sd_Temp_chest,
+    min_Temp_chest = min_Temp_chest,
+    max_Temp_chest = max_Temp_chest,
+    range_Temp_chest = max_Temp_chest - min_Temp_chest
+  )
+  
   # adding dataframes to list
   EDA_chest_list[[i]] <- EDA_chest_df
   EDA_wrist_list[[i]] <- EDA_wrist_df
   EMG_list[[i]] <- EMG_df
+  Temp_chest_list[[i]] <- Temp_chest_df
+  Temp_wrist_list[[i]] <- Temp_wrist_df
 }
 
 # bind rows for all dataframes (each dataframe is a subject)
 EDA_chest_eng <- do.call(bind_rows, EDA_chest_list)
 EDA_wrist_eng <- do.call(bind_rows, EDA_wrist_list)
 EMG_eng <- do.call(bind_rows, EMG_list)
+Temp_chest_eng <- do.call(bind_rows, Temp_chest_list)
+Temp_wrist_eng <- do.call(bind_rows, Temp_wrist_list)
 
 # add all of our engineered values/dataframes into here 
 final_data <- bind_cols(EDA_chest_eng, 
                         EDA_wrist_eng %>% select(-participant, -label), 
-                        EMG_eng %>% select(-participant, -label))
+                        EMG_eng %>% select(-participant, -label), 
+                        Temp_chest_eng %>% select(-participant, -label),
+                        Temp_wrist_eng %>% select(-participant, -label))
 
 #saveRDS(final_data, file = "final_data.Rds")
  
