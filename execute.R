6ef09be7d9fe50b6332fbe6ea2255e27d1587c87
Scott Heng On <sdh45@duke.edu>
2020-09-29 19:07:03 -0400

    added some EDA

--- execute.R
+++
@@ -1,5 +0,0 @@
-# This R script includes all necessary code reproducibility to be done for STA 440 Case Study 2 (Detecting Stress)
-# This R script can be run on the cluster by:
-#               1)
-#               2)
-#               3)
--- submit_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --account=sta440-f20
-#SBATCH -p common
-#SBATCH -N1
-#SBATCH -c1
-#SBATCH --mem=1G
-
-python data.py
